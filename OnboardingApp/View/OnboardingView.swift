8b29b5078dcff9428ee9f7b12bb8521645b69970
dan1x <167653251+dan1xdev@users.noreply.github.com>
2025-01-18 00:25:31 +0300

    Update OnboardingView.swift

--- OnboardingApp/View/OnboardingView.swift
+++ OnboardingApp/View/OnboardingView.swift
@@ -3,49 +3,48 @@ import UIKit
 
 struct OnboardingView: View {
     @StateObject private var viewModel = OnboardingViewModel()
-    @State var currentPage = 0 // Текущая страница
-    private let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)  // Создаем генератор вибрации
+    @State var currentPage = 0 
+    private let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium) 
     init() {
-        feedbackGenerator.prepare() // Подготовка к вибрации
+        feedbackGenerator.prepare() 
     }
     var body: some View {
         
         ZStack {
-            // TabView для свайпов
             TabView(selection: $viewModel.currentPage) {
                 ForEach(viewModel.pages.indices, id: \.self) { index in
                     OnboardingPageView(
                         OnboardingPage: viewModel.pages[index],
                         currentPage: $viewModel.currentPage
                     )
-                    .tag(index) // Установите теги для отслеживания текущей страницы
+                    .tag(index) 
                     .onChange(of: viewModel.currentPage) {
-                        feedbackGenerator.impactOccurred() // Вибрация при изменении страницы
+                        feedbackGenerator.impactOccurred() 
                     }
                     
                 }
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never)) // Отключение стандартного индикатора
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never)) 
             
-            // Кастомный индикатор
+
             HStack {
                 ForEach(0..<4) { index in
                     if viewModel.currentPage == index {
                         Capsule()
                             .frame(width: 27, height: 10)
-                            .foregroundColor(Color.mywhite) // Акцентный цвет приложения
+                            .foregroundColor(Color.mywhite) 
                             .animation(.easeInOut, value: viewModel.currentPage)
                     } else {
                         Circle()
                             .frame(width: 10, height: 10)
-                            .foregroundColor(Color.otherIcons) // Серый цвет для неактивных точек
+                            .foregroundColor(Color.otherIcons)
                             .animation(.easeInOut, value: viewModel.currentPage)
                     }
                 }
             }
-            .position(x: 70, y: UIScreen.main.bounds.height - 100) // Позиционирование
+            .position(x: 70, y: UIScreen.main.bounds.height - 100) 
         }
-        .ignoresSafeArea() // Игнорирование безопасных областей экрана
+        .ignoresSafeArea()
     }
 }
 
